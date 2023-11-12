3e42e0e2f8b4fa59dfa36f48b311eaff7e6cf120
theaveasso <theaveas.so@gmail.com>
2023-11-12 19:08:49 +0700

    background

--- src/main.js
+++ src/main.js
@@ -1,60 +1,74 @@
-let playerState = "run";
-document.getElementById("playerState").onchange = function(e) {
-  playerState = e.target.value;
-};
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-const canvasWidth = (canvas.width = 600);
-const canvasHeight = (canvas.height = 600);
+const canvasWidth = (canvas.width = 800);
+const canvasHeight = (canvas.height = 700);
 
-const img = new Image();
-img.src = "../elements/Shadow Dog.png";
+const bgL1 = new Image();
+bgL1.src = "../elements/environment-bg/layer-1.png";
+const bgL2 = new Image();
+bgL2.src = "../elements/environment-bg/layer-2.png";
+const bgL3 = new Image();
+bgL3.src = "../elements/environment-bg/layer-3.png";
+const bgL4 = new Image();
+bgL4.src = "../elements/environment-bg/layer-4.png";
+const bgL5 = new Image();
+bgL5.src = "../elements/environment-bg/layer-5.png";
 
-const sw = 575;
-const sh = 523;
-
-let gameFrame = 0;
-let staggerFrame = 7;
-
-const animationStates = [
-  { name: "idle", frame: 7 },
-  { name: "jump", frame: 7 },
-  { name: "fall", frame: 7 },
-  { name: "run", frame: 9 },
-  { name: "dizzy", frame: 11 },
-  { name: "sit", frame: 5 },
-  { name: "roll", frame: 7 },
-  { name: "bite", frame: 7 },
-  { name: "ko", frame: 12 },
-  { name: "getHit", frame: 4 },
-];
-
-const spriteAnimations = Object.assign(
-  {},
-  ...animationStates.map((state, i) => {
-    const frames = {
-      loc: [],
-    };
-    for (let j = 0; j < state.frame; j++) {
-      let positionX = j * sw;
-      let positionY = i * sh;
-      frames.loc.push({ x: positionX, y: positionY });
+window.addEventListener("load", () => {
+  let gameFrame = 0;
+  let gameSpeed = 5;
+  document.getElementById("gameSpeed").addEventListener("input", (e) => {
+    gameSpeed = e.target.value;
+  });
+  class Layer {
+    constructor(image, speedModifier) {
+      this.width = 2400;
+      this.height = 700;
+      this.y = 0;
+      this.x = 0;
+      this.image = image;
+      this.speedModifier = speedModifier;
+      this.speed = gameSpeed * this.speedModifier;
     }
-    return { [state.name]: frames };
-  }),
-);
 
-function animate() {
-  ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-  let pos =
-    Math.floor(gameFrame / staggerFrame) %
-    spriteAnimations[playerState].loc.length;
-  const locX = spriteAnimations[playerState].loc[pos].x;
-  const locY = spriteAnimations[playerState].loc[pos].y;
-  ctx.drawImage(img, locX, locY, sw, sh, 0, 0, sw, sh);
-  gameFrame++;
-  requestAnimationFrame(animate);
-}
+    update() {
+      this.speed = gameSpeed * this.speedModifier;
+      // this.x = (gameFrame * this.speed) % this.width;
+      if (this.x <= -this.width) {
+        this.x = 0;
+      }
+      this.x = Math.floor(this.x - this.speed);
+    }
 
-animate();
+    draw() {
+      ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+      ctx.drawImage(
+        this.image,
+        this.x + this.width,
+        this.y,
+        this.width,
+        this.height,
+      );
+    }
+  }
+
+  const layer1 = new Layer(bgL1, 0.3);
+  const layer2 = new Layer(bgL2, 0.2);
+  const layer3 = new Layer(bgL3, 0.1);
+  const layer4 = new Layer(bgL4, 0.5);
+  const layer5 = new Layer(bgL5, 1);
+
+  const layers = [layer1, layer2, layer3, layer4, layer5];
+  function animate() {
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+    layers.forEach((layer) => {
+      layer.update();
+      layer.draw();
+    });
+    gameFrame--;
+    requestAnimationFrame(animate);
+  }
+
+  animate();
+});
